f327b405862580b86d42f6f1dedae087faacef0d
pavelsukagunas <paveleframsukagunas@mail.ugm.ac.id>
2026-06-16 21:15:31 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class="top-footer">
+    <h2><i class="bx bx-envelope"></i>Sign Up For Updates</h2>
+    <div class="input-field">
+        <input type="text" name="" placeholder="email address...">
+        <button class="btn">subscribe</button>
+    </div>
+</div>
+<footer>
+    <div class="overlay"></div>
+    <div class="footer-content">
+        <div class="img-box">
+            <img src="img/logo2.png">
+        </div>
+        <div class="bottom-footer">
+            <p>2026 puffgrammer</p>
+        </div>
+    </div>
+</footer>
